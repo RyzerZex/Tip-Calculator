3f0ee0850e8f40d70c48eaef938e4e4a99630028
RyzerZex <muhammadsafdar3349@gmail.com>
2024-11-20 23:31:56 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [bill, setBill] = useState(null);
+  const [tip, setTip] = useState(0.1);
+  const [friendTip, setFriendTip] = useState(0.1);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BillInput bill={bill} onSetBill={setBill}></BillInput>
+      <TipInput tip={tip} onSetTip={setTip}>
+        How did you like the service :
+      </TipInput>
+      <TipInput tip={friendTip} onSetTip={setFriendTip}>
+        How did your friend like the service :
+      </TipInput>
+      <FinalBill bill={bill} tip={tip} friendTip={friendTip}></FinalBill>
     </div>
   );
 }
+function BillInput({ bill, onSetBill }) {
+  return (
+    <div className="container">
+      <span>
+        How much was the bill:
+        <input
+          type="number"
+          value={bill}
+          onChange={(e) => onSetBill(e.target.value)}
+        ></input>
+      </span>
+    </div>
+  );
+}
+function TipInput({ children, tip, onSetTip }) {
+  function handleTip(e) {
+    //const x = Number(e.target.value) / 100;
+    onSetTip(e.target.value);
+  }
+  return (
+    <div className="container">
+      <span>
+        {children}
+        <select value={tip} onChange={(e) => handleTip(e)}>
+          <option value={0.1}>not satisfied 10%</option>
+          <option value={0.15}>satisfied 15%</option>
+          <option value={0.2}>Great 20%</option>
+        </select>
+      </span>
+    </div>
+  );
+}
+function FinalBill({ bill, tip, friendTip }) {
+  const yourTip = bill * tip;
+  const yourFriendTip = bill * friendTip;
+  const totalBill = Number(bill) + yourTip + yourFriendTip;
 
-export default App;
+  return (
+    <div className="result">
+      <h1>
+        You pay ${totalBill.toFixed(2)} (${bill} + ${yourTip.toFixed(2)} (your
+        tip) + ${yourFriendTip.toFixed(2)} (friend's tip))
+      </h1>
+    </div>
+  );
+}
